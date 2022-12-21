1114a7747c99caa35cd5e7e956e2e8d1260c4456
ArmaanSinghBrar <120672171+ArmaanSinghBrar@users.noreply.github.com>
2022-12-21 18:52:40 +0530

    Update Question 2.py

--- Question 2.py
+++ Question 2.py
@@ -1,9 +1,9 @@
 #Calculating a person's income tax
 Income = float(input("Enter your gross income(to the nearest penny) ="))
 Dependents = int(input("Enter number of dependents="))
-a = Income - 10000 -(Dependents*3000)    #a is the taxable income
-tax = a*0.2    #total income tax
-if(a>0):
-    print("Your income tax =$",tax)
+taxable_income = Income - 10000 - (Dependents*3000)
+tax = taxable_income*0.2    #total income tax
+if(taxable_income>0):
+    print("Your income tax = $",tax)
 else:
     print("Your income tax = $0")    #If the total taxable income is 0 then the person has 0 income tax
