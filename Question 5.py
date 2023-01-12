a37b8d389ababac27684b584425e89ecb704db18
ArmaanSinghBrar <120672171+ArmaanSinghBrar@users.noreply.github.com>
2023-01-12 15:29:30 +0530

    Update Question 5.py

--- Question 5.py
+++ Question 5.py
@@ -1,8 +1,6 @@
 #Sine and Cosine values between 0 and 345 with 15 increments
 import math
-pie = 3.14
-i = 0
-for i in range(0,346,15) :
-     a = math.sin(math.radians(i))    #a is the values of sine function
-     b = math.cos(math.radians(i))    #b is the values of cosine function
-     print( i , " --- " , round(a,4),round(b,4))
+for i in range(0,346,15):
+    Sine = math.sin(math.radians(i))
+    Cosine = math.cos(math.radians(i))
+    print(i,"---",round(Sine,4),round(Cosine,4))
