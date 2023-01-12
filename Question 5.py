8ca491bc38990cc68209566bcec53a02e989089f
ArmaanSinghBrar <120672171+ArmaanSinghBrar@users.noreply.github.com>
2023-01-12 15:30:42 +0530

    Update Question 5.py

--- Question 5.py
+++ Question 5.py
@@ -1,6 +1,6 @@
 #Sine and Cosine values between 0 and 345 with 15 increments
 import math
-for i in range(0,346,15):
+for i in range(0,346,15):                          #running a loop till 345 with 15 increments
     Sine = math.sin(math.radians(i))
     Cosine = math.cos(math.radians(i))
-    print(i,"---",round(Sine,4),round(Cosine,4))
+    print(i,"---",round(Sine,4),round(Cosine,4))        #Rounding value for Sine and Cosine till 4 decimal places
